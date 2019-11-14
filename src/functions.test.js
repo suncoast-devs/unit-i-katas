ac641c051202283df50dd858f3c3e90c59f8fec7
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-14 08:45:03 -0500

    added the tests

--- src/functions.test.js
+++ src/functions.test.js
@@ -28,7 +28,7 @@
 
 /**
  *
- * 3) Define a function called check that takes 2 arguments, an array and a value. 
+ * 3) Define a function called doesContain that takes 2 arguments, an array and a value. 
  *    Check whether the provided array contains the value. Array can contain numbers or strings.
       Your function should return true if the array contains the value, false if not.
  */
@@ -50,7 +50,7 @@
  *    For example: 
  *    noSpace("This is a my cool thing") should return "Thisisamycoolthing"
 
- 
+
 /**
  * NOTE: Don't modify anything below this line...
  */
@@ -66,198 +66,51 @@ const ensureDefined = (t, method) => {
     )
   }
 }
+test('sum()', t => {
+  ensureDefined(t, 'sum')
 
-test('Function Check', t => ensureDefined(t, 'yelling'))
-test('yelling()', t => {
-  t.deepEqual(yelling(['now', 'is', 'the', 'time']), [
-    'NOW',
-    'IS',
-    'THE',
-    'TIME'
-  ])
+  t.is(sum(8, 11), 19)
+  t.is(sum(4, 100), 104)
 })
 
-test('Function Check', t => ensureDefined(t, 'doubleTrouble'))
-test('doubleTrouble()', t => {
-  t.deepEqual(doubleTrouble([2, 3, 9, 0, -5]), [4, 6, 18, 0, -10])
+test('sumOfArray()', t => {
+  ensureDefined(t, 'sumOfArray')
+
+  t.is(sumOfArray([1, 2]), 3)
+  t.is(sumOfArray([1, 2, 3]), 6)
+  t.is(sumOfArray([10, 9, 8]), 27)
+  t.is(sumOfArray([]), 0)
 })
 
-test('Function Check', t => ensureDefined(t, 'stringyIndexes'))
-test('stringyIndexes', t => {
-  t.deepEqual(stringyIndexes(['how', 'now', 'brown', 'cow']), [
-    'how is at index 0',
-    'now is at index 1',
-    'brown is at index 2',
-    'cow is at index 3'
-  ])
-})
+test('doesContain()', t => {
+  ensureDefined(t, 'doesContain')
 
-test('Function Check', t => ensureDefined(t, 'onlyTheEvenSurvive'))
-test('onlyTheEvenSurvive', t => {
-  t.deepEqual(onlyTheEvenSurvive([42, 50, 100, 5, -43, 17, 44]), [
-    42,
-    50,
-    100,
-    44
-  ])
-})
-
-test('Function Check', t => ensureDefined(t, 'onlyTheEvenIndexedSurvive'))
-test('onlyTheEvenIndexedSurvive', t => {
-  t.deepEqual(
-    onlyTheEvenIndexedSurvive([
-      31,
-      67,
-      64,
-      96,
-      14,
-      24,
-      43,
-      51,
-      48,
-      80,
-      58,
-      43,
-      64,
-      84,
-      98,
-      99,
-      69,
-      93,
-      5,
-      32,
-      29,
-      4,
-      28,
-      18,
-      86,
-      22,
-      20,
-      74,
-      35,
-      27,
-      85,
-      79,
-      65,
-      32,
-      56,
-      94,
-      93,
-      20,
-      29,
-      22,
-      72
-    ]),
-    [
-      31,
-      64,
-      14,
-      43,
-      48,
-      58,
-      64,
-      98,
-      69,
-      5,
-      29,
-      28,
-      86,
-      20,
-      35,
-      85,
-      65,
-      56,
-      93,
-      29,
-      72
-    ]
-  )
-})
-
-test('Function Check', t => ensureDefined(t, 'bestMoviesOfTheYear'))
-test('bestMoviesOfTheYear', t => {
-  const movies = [
-    { name: 'The Grand Budapest Hotel', year: 2014, score: 91 },
-    { name: 'Birdman', year: 2014, score: 91 },
-    { name: 'Transformers: Age of Extinction', year: 2014, score: 18 },
-    { name: 'Rage', year: 2014, score: 14 },
-    { name: 'Get Out', year: 2017, score: 99 },
-    { name: 'Justice League', year: 2017, score: 40 },
-    { name: 'Ghost in the Shell', year: 2017, score: 46 },
-    { name: 'The Big Sick', year: 2017, score: 98 }
-  ]
-
-  t.deepEqual(bestMoviesOfTheYear(movies, 2014), [
-    'The Grand Budapest Hotel',
-    'Birdman'
-  ])
-
-  t.deepEqual(bestMoviesOfTheYear(movies, 2017), ['Get Out', 'The Big Sick'])
-
-  t.deepEqual(bestMoviesOfTheYear(movies, 2001), [])
-})
-
-test('Function Check', t => ensureDefined(t, 'everyoneIsOdd'))
-test('everyoneIsOdd', t => {
-  t.is(everyoneIsOdd([9, 15, 27, 101, 33]), true)
-  t.is(everyoneIsOdd([9, 23, 3, 4, 77]), false)
-})
-
-test('Function Check', t => ensureDefined(t, 'findTheNeedle'))
-test('findTheNeedle', t => {
+  t.is(doesContain([66, 101], 66), true)
+  t.is(doesContain([80, 117, 115, 104, 45, 85, 112, 115], 45), true)
+  t.is(doesContain([80, 117, 115, 104, 45, 85, 112, 115], '45'), false)
+  t.is(doesContain(['t', 'e', 's', 't'], 'e'), true)
+  t.is(doesContain(['what', 'a', 'great', 'kata'], 'kat'), false)
   t.is(
-    findTheNeedle(['one', 'time', 'there was a needle at', 'the market']),
-    'there was a needle at'
+    doesContain(['this', 'should', 3, 7, 3, 'still', 'work'], 'should'),
+    true
   )
 })
 
-test('Function Check', t => ensureDefined(t, 'findTheNeedleIndex'))
-test('findTheNeedleIndex', t => {
+test('maps()', t => {
+  ensureDefined(t, 'maps')
+
+  t.deepEqual(maps([1, 2, 3]), [2, 4, 6])
+  t.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8])
+  t.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4])
+})
+
+test('noSpace()', t => {
+  ensureDefined(t, 'noSpace')
+  t.is(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB')
   t.is(
-    findTheNeedleIndex(['one', 'time', 'there was a needle at', 'the market']),
-    2
+    noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'),
+    '88Bifk8hB8BB8BBBB888chl8BhBfd'
   )
+  t.is(noSpace('8aaaaa dddd r     '), '8aaaaaddddr')
 })
-
-test('Function Check', t => ensureDefined(t, 'someoneToLove'))
-test('someoneToLove()', t => {
-  t.is(someoneToLove(['how', 'now', 'brown', 'cow']), false)
-  t.is(someoneToLove(['how', 'now', 'blue', 'cow']), true)
-})
-
-test('Function Check', t => ensureDefined(t, 'mapYourself'))
-test('mapYourself()', t => {
-  const originalMap = Array.prototype.map
-
-  Array.prototype.map = () => []
-
-  t.deepEqual(mapYourself([1, 2, 3]), [2, 4, 6])
-  t.deepEqual(mapYourself([9, 0, 1]), [18, 0, 2])
-
-  Array.prototype.map = originalMap
-})
-
-test('Function Check', t => ensureDefined(t, 'filterYourself'))
-test('filterYourself()', t => {
-  const original = Array.prototype.filter
-
-  Array.prototype.filter = () => []
-
-  t.deepEqual(filterYourself([8, 1, 2, 3]), [8, 2])
-
-  Array.prototype.filter = original
-})
-
-test('Function Check', t => ensureDefined(t, 'everyYourself'))
-test('everyYourself()', t => {
-  const original = Array.prototype.every
-
-  Array.prototype.every = () => undefined
-
-  t.deepEqual(everyYourself([8, 1, 2, 3]), false)
-  t.deepEqual(everyYourself([8, 10, 22, 38]), true)
-
-  Array.prototype.every = original
-})
-
 /* eslint-enable */
